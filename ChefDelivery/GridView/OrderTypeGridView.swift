bfdae6503a5e8f0d827b485e20ace48c8aabb03a
Dithan <thiagodesousarocha1311@gmail.com>
2024-08-24 12:56:13 -0300

    Commit inicial

---
+++ ChefDelivery/GridView/OrderTypeGridView.swift
@@ -0,0 +1,18 @@
+//
+//  OrderTypeGridView.swift
+//  ChefDelivery
+//
+//  Created by Thiago Rocha on 24/08/24.
+//
+
+import SwiftUI
+
+struct OrderTypeGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OrderTypeGridView()
+}
